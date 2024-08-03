2a4b75c7ea3aff3daa122f17c7b549feff0a1fcf
Bailey Benoit <Bailey.L.Benoit@gmail.com>
2024-08-03 10:47:09 -0700

    updated fetch request test

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
     fetchData();
 });
+
+function fetchData(searchTerm = 'margarita') {
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchTerm}`)
+    .then((resp) => resp.json())
+    .then((data) => {
+        displayCocktails(data.drinks);
+    })
+    .catch((error) => {
+        console.error('Error fetching data:', error);
+        cocktailList.innerHTML = '<p>No results found. Try another search!</p>';
+    });
+}
