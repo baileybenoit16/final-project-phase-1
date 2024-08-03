3fafa8b1cbeadace3a2094fe59353d4adab279af
Bailey Benoit <Bailey.L.Benoit@gmail.com>
2024-08-03 10:45:40 -0700

    fetch data test

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-document.addEventListener("DOMContentLoaded", fetchData)
-
-function fetchData() {
-    fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
-    .then((resp) => resp.json())
-    .then((data) => console.log(data))
-}
-
+document.addEventListener("DOMContentLoaded", () => {
+    fetchData();
+});
